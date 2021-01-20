5ca89d96f94d44ed108fe00fce6e1c15bbe2703b
Lucas Klis <lukaszklis.poczta@gmail.com>
2021-01-21 00:39:04 +0100

    1dzień

---
+++ plik_ze_skryptem.js
@@ -0,0 +1,22 @@
+/*przykład komentarza wieloliniowego
+przykłądu ciąg dalszy
+*/
+console.log('To jest mój pierwszy skrypt')
+
+//przykład jednolinowego komentarza
+
+console.log('To jest mój pierwszy skrypt')
+
+//komentarze warto uzupełniać informacją TODO i FIXME
+alert ("Przed Tobą naprawdę ważne pytanie")
+if (confirm("Czy jesteś pewien, że chcesz kontynuować?")) {alert("No to kontynuuj")
+} else {alert("Przykro mi, że nie chcesz kontynuować...")
+}
+const name = prompt("Podaj swoje imię:");
+
+if (name) {
+    alert("Witaj " + name)
+} else {
+    alert("Anulowałeś akcję, a okienko zwróciło " + name)
+}
+    
